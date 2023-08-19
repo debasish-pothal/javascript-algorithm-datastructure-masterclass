d697bc9d9f6ddd759ad87024c2203252dfb919e7
Debasish Pothal <debasish_pothal@hotmail.com>
2023-08-19 11:56:18 +0530

    added graph

---
+++ graph.js
@@ -0,0 +1,41 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(v) {
+    if (!this.adjacencyList[v]) {
+      this.adjacencyList[v] = [];
+    }
+  }
+
+  removeVertex(v) {
+    this.adjacencyList[v].forEach((tempV) => {
+      this.removeEdge(v, tempV);
+    });
+
+    delete this.adjacencyList[v];
+  }
+
+  addEdge(v1, v2) {
+    if (!this.adjacencyList[v1]) {
+      this.addVertex(v1);
+    }
+
+    if (!this.adjacencyList[v2]) {
+      this.addVertex(v2);
+    }
+
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
+}
+
+const g = new Graph();
+
+console.log(g);
